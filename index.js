f34fea6c0f96a2de66ea5a4ce9e7dd52b294e9f1
Wesley da Silva <wesleyysilvaa1912@gmail.com>
2024-07-07 19:10:56 -0300

    configuring my imports

--- index.js
+++ index.js
@@ -3,5 +3,12 @@ const exphbs = require('express-handlebars')
 const session = require('express-session')
 const fileStore = require('session-file-store')(session)
 const flash = require('express-flash')
+const port = 3000
 
 const app = express()
+
+const conn = require('./db/conn')
+
+conn.sync().then(() => {
+    app.listen(port)
+}).catch((err) => console.log(err))
