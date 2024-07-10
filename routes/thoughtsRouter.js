f1fb116901a7675965bfe92b3c513562a12bb754
Wesley da Silva <wesleyysilvaa1912@gmail.com>
2024-07-09 21:45:56 -0300

    routes

---
+++ routes/thoughtsRouter.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const router = express.Router()
+const ThoughtsController = require('..')
+
+router.get('/', ThoughtsController.showThoughts)
+
+module.exports = router
